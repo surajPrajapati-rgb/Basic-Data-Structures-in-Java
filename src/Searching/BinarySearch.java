bc9d90cf0aa0c800183af880ce5648589236f741
= <jnvsuraj7388@gmail.com>
2024-02-26 21:19:21 +0530

    Initialized the basic data structure in java
    Signed-off-by: = <jnvsuraj7388@gmail.com>

--- src/Searching/BinarySearch.java
+++ src/Searching/BinarySearch.java
@@ -1,24 +1,24 @@
 package Searching;
 
 class BinarySearch {
-    int binarySearch(int array[], int element, int low, int high) {
+    int binarySearch(int arr[], int element, int low, int high) {
         while (low <= high) {
             int mid = low + (high - low) / 2;
-            if (array[mid] == element)
+            if (arr[mid] == element)
                 return mid;
-            if (array[mid] < element)
+            if (arr[mid] < element)
                 low = mid + 1;
             else
                 high = mid - 1;
         }
         return -1;
     }
-    int binarySearch(double array[], double element, int low, int high) {
+    int binarySearch(double arr[], double element, int low, int high) {
         while (low <= high) {
             int mid = low + (high - low) / 2;
-            if (array[mid] == element)
+            if (arr[mid] == element)
                 return mid;
-            if (array[mid] < element)
+            if (arr[mid] < element)
                 low = mid + 1;
             else
                 high = mid - 1;
---
+++ src/Sorting/SelectionSort.java
@@ -0,0 +1,24 @@
+package Sorting;
+
+class SelectionSort {
+    int[]  selectionSort(int[] arr) {
+        int n = arr.length;
+        // One by one move the boundary of the unsorted subarr
+        for (int i = 0; i < n - 1; i++) {
+            // Find the minimum element in the unsorted arr
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            // Swap the found minimum element with the first element
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+        return arr;
+    }
+
+
+}
