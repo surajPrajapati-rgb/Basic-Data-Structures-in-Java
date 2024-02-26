73faff62af66a973fa001de195b8a5258e40f2ff
= <jnvsuraj7388@gmail.com>
2024-02-26 19:36:48 +0530

    Initialized the basic data structure in java

---
+++ src/Searching/BinarySearch.java
@@ -0,0 +1,28 @@
+package Searching;
+
+class BinarySearch {
+    int binarySearch(int array[], int element, int low, int high) {
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (array[mid] == element)
+                return mid;
+            if (array[mid] < element)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return -1;
+    }
+    int binarySearch(double array[], double element, int low, int high) {
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (array[mid] == element)
+                return mid;
+            if (array[mid] < element)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return -1;
+    }
+}
---
+++ src/Searching/LinearSearch.java
@@ -0,0 +1,22 @@
+package Searching;
+
+public class LinearSearch {
+    public int linearSearch(int[] arr, int key){
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key){
+                return i;
+            }
+        }
+        return -1; // not found
+    }
+    public int linearSearch(double[] arr, double key){
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key){
+                return i;
+            }
+
+        }
+        return -1; // not found
+    }
+
+}
