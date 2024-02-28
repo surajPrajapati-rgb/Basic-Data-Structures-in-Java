1be7b4093ff02b4384f2bf5e955d9c093b90de6e
= <jnvsuraj7388@gmail.com>
2024-02-28 18:42:52 +0530

    updated stack and Queue
    Signed-off-by: = <jnvsuraj7388@gmail.com>

--- src/Queue/Queue.java
+++ src/Queue/Queue.java
@@ -1,4 +1,66 @@
 package Queue;
-
 public class Queue {
+    int SIZE = 5;
+    int[] items = new int[SIZE];
+    int front, rear;
+
+    Queue() {
+        front = -1;
+        rear = -1;
+    }
+    boolean isFull() {
+        if (front == 0 && rear == SIZE - 1) {
+            return true;
+        }
+        return false;
+    }
+
+    boolean isEmpty() {
+        return front == -1;
+    }
+    void enQueue(int element) {
+        if (isFull()) {
+            System.out.println("Queue is full");
+        }
+        else {
+            if (front == -1) {
+                front = 0;
+            }
+            rear++;
+            items[rear] = element;
+            System.out.println("Insert " + element);
+        }
+    }
+    int deQueue() {
+        int element;
+        if (isEmpty()) {
+            System.out.println("Queue is empty");
+            return (-1);
+        }
+        else {
+            element = items[front];
+            if (front >= rear) {
+                front = -1;
+                rear = -1;
+            }
+            else {
+                front++;
+            }
+            System.out.println( element + " Deleted");
+            return (element);
+        }
+    }
+    void display() {
+        int i;
+        if (isEmpty()) {
+            System.out.println("Empty Queue");
+        }
+        else {
+            System.out.println("\nFront index-> " + front);
+            System.out.println("Items -> ");
+            for (i = front; i <= rear; i++)
+                System.out.print(items[i] + " ");
+            System.out.println("\nRear index-> " + rear);
+        }
+    }
 }
--- src/Stack/Stack.java
+++ src/Stack/Stack.java
@@ -1,5 +1,46 @@
 package Stack;
 
 public class Stack {
-
+    int n;
+    int[] arr;
+    int top;
+    Stack(int n) {
+        this.n = n;
+        this.arr = new int[n];
+        this.top = -1;
+    }
+    public boolean isEmpty() {
+        return (top == -1);
+    }
+    public boolean isFull() {
+        return (n - 1 == top);
+    }
+    public void push(int pushedElement) {
+        if (!isFull()) {
+            top++;
+            arr[top] = pushedElement;
+            System.out.println("Pushed element:" + pushedElement);
+        } else {
+            System.out.println("Stack is full !");
+        }
+    }
+    public int pop() {
+        if (!isEmpty()) {
+            int poppedElement = top;
+            top--;
+            System.out.println("Popped element :" + arr[poppedElement]);
+            return arr[poppedElement];
+        } else {
+            System.out.println("Stack is empty!");
+            return -1;
+        }
+    }
+    public int peek() {
+        if (!this.isEmpty())
+            return arr[top];
+        else {
+            System.out.println("Stack is Empty");
+            return -1;
+        }
+    }
 }
